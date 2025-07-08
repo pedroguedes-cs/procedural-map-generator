0312b08ba37843bad21c5ccab4eb7ceb2c37619c
Arthur <arthurronaldpdm@gmail.com>
2025-07-08 10:26:35 -0300

    funcoes_finalizadas

--- src/imagem.cpp
+++ src/imagem.cpp
@@ -7,8 +7,12 @@ using namespace std;
 
 Imagem::Imagem(int largura1, int altura1)
 {
-    for(int i=0; i < altura1; ++i){
-        for(int j=0; j < largura1; ++j){
+    altura = altura1;
+    largura = largura1;
+    matriz = new Cor[altura1 * largura1];
+
+    for(int i=0; i < altura1; i++){
+        for(int j=0; j < largura1; j++){
             matriz[i * largura1 + j] = {0, 0, 0};
         }
     }
@@ -16,14 +20,17 @@ Imagem::Imagem(int largura1, int altura1)
 
 Cor Imagem::consulta_pixel(int largura1, int altura1)
 {
-    return matriz[altura1 * largura + largura1];   
+    Cor resposta = matriz[altura1 * largura + largura1];
+    return resposta;   
 }
 
-void Imagem::definir_cor(int largura1, int altura1, Cor cor_pixel)
+/*void Imagem::definir_cor(int largura1, int altura1, Cor cor_pixel)
 {
     *(matriz + (altura1 * largura) + largura1)
-}
+}*/
 
 void salvar_imagem();
 
-void liberar_matriz();
+void Imagem::liberar_matriz(Imagem imagem){
+    delete[] imagem.matriz;
+};
