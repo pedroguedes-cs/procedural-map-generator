b5f1198a18702454b522203a3ec623309f4f721d
Pedro Guedes <pgdtsouza@gmail.com>
2025-07-08 09:38:13 -0300

    Método definir_cor()

--- src/imagem.cpp
+++ src/imagem.cpp
@@ -19,7 +19,9 @@ Cor Imagem::consulta_pixel(int largura1, int altura1)
 
 void Imagem::definir_cor(int largura1, int altura1, Cor cor_pixel)
 {
-    *(matriz + (altura1 * largura) + largura1)
+    matriz[altura1 * largura + largura1].r = cor_pixel.r; 
+    matriz[altura1 * largura + largura1].g = cor_pixel.g; 
+    matriz[altura1 * largura + largura1].b = cor_pixel.b; 
 }
 
 void salvar_imagem();
