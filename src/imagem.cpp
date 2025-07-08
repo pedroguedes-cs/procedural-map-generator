a431ea26b96e45420a5564fa0f189fb844d04164
Arthur <arthurronaldpdm@gmail.com>
2025-07-08 09:18:21 -0300

    alteracao funcoes

--- src/imagem.cpp
+++ src/imagem.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 Imagem::Imagem(int largura1, int altura1)
 {
-
+    for(int i=0; i < altura1; ++i){
+        for(int j=0; j < largura1; ++j){
+            matriz[i * largura1 + j] = {0, 0, 0};
+        }
+    }
 }
 
 Cor Imagem::consulta_pixel(int largura1, int altura1)
 {
-
+    Cor resposta;
+    
 }
 
 void Imagem::definir_cor(int largura1, int altura1, Cor cor_pixel)
