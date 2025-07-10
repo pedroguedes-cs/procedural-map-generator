84897e852eec29a12c8cecb8bd0eabd5d16a8a93
Arthur <arthurronaldpdm@gmail.com>
2025-07-10 10:26:37 -0300

    funcoes etapa3

--- src/terreno.cpp
+++ src/terreno.cpp
@@ -1,19 +1,28 @@
 #include "terreno.h"
 #include <string>
-
+#include <fstream>
+#include <cmath>
 using namespace std;
 
 
 //Construtor padrão
 Terreno::Terreno(int gerador)
-{
+{   
+    int tamanho = pow(2, gerador) + 1;
+    linhas = tamanho;
+    colunas = tamanho;
+    altitudes = new int[tamanho * tamanho];
+
+    for(int i=0; i < tamanho; i++){
+        altitudes[i] = 0;
+    }
 
 }
 
 //Destrutor
 Terreno::~Terreno()
 {
-
+    delete[] altitudes;
 }
 
 //Gerar mapa
@@ -25,25 +34,43 @@ void Terreno::gerar_mapa(int gerador, double rugosidade)
 //Consultar linhas
 int Terreno::consulta_linhas()
 {
-
+    return linhas;
 }
 
 //Consultar colunas
 int Terreno::consulta_colunas()
 {
-
+    return colunas;
 }
 
 //Consultar altitude
 int Terreno::consulta_altitude(int linha1, int coluna1)
 {
-
+    return altitudes[linhas * colunas + coluna1];
 }
 
 //Salvar terreno em um arquivo
 void Terreno::salvar_terreno(string nome_arquivo)
 {
+    ofstream arquivo(nome_arquivo, ios::out | ios::trunc);
 
+    if(arquivo.is_open() == false)
+    {
+        return;
+    }
+    else
+    {
+        arquivo << linhas << " " << colunas << endl;
+    }
+
+    for(int i=0; i < linhas; i++)
+    {
+        for(int j=0; j < colunas; j++)
+        {
+            arquivo << altitudes[i * colunas + j] << " ";
+        }
+        arquivo << endl;
+    }
 }
 
 //Ler um arquivo e retornar um terreno
