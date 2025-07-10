2b29f9aa78df949aaf545a4bed310bff37aafc82
Pedro Guedes <pgdtsouza@gmail.com>
2025-07-10 20:03:06 -0300

    Teste gerar_mapa()

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 #include "../include/imagem.h"
 #include "../include/paleta.h"
 #include "../include/terreno.h"
@@ -7,9 +8,24 @@
 using namespace std;
 
 int main()
-{   
-    Terreno teste(3, 0.5);
+{  
+    int gerador, tamanho_linha;
+    cout << "Digite um número gerador: ";
+    cin >> gerador;
 
-    cout << teste.consulta_altitude(0, 1);
+    tamanho_linha = pow(2, gerador) + 1;
 
+    Terreno teste(gerador, 0.5);
+
+    for (int i = 0; i < (tamanho_linha * tamanho_linha); i++)
+    {
+        if (i % tamanho_linha == tamanho_linha - 1)
+        {
+            cout << teste.consulta_altitude(i / tamanho_linha, i % tamanho_linha) << endl;
+        }
+        else
+        {
+            cout << teste.consulta_altitude(i / tamanho_linha, i % tamanho_linha) << " ";
+        }
+    }
 }
