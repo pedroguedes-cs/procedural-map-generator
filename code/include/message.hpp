1383a5414548ae16c98e461d5a28b47b66a24bce
Pedro <pgdtsouza@gmail.com>
2025-09-01 17:50:50 -0300

    Messages

--- code/include/message.hpp
+++ code/include/message.hpp
@@ -1,6 +1,6 @@
 #pragma once 
 
-void info_saved();
+void saved();
 
 void image_generated();
 
---
+++ code/src/message.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "../include/message.hpp"
+
+void saved()
+{
+    std::cout << "\nYour changes were saved.\n";
+}
+
+void image_generated()
+{
+    std::cout << "Your image has been created.\n";
+}
+
+void exiting()
+{
+      std::cout << "Exiting program...\n";
+}
+
+void bye()
+{
+    std::cout << "Bye! See you next time.\n\n\n\n";
+}
